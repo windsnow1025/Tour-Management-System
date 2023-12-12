fd972b6e2f54acee3080a45a2c3065ea8dc08021
windsnow1025 <windsnow1025@gmail.com>
2023-12-12 05:42:47 +0000

    spring init

---
+++ src/main/java/com/windsnow1025/tourmanagementsystem/TourManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.windsnow1025.tourmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TourManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/windsnow1025/tourmanagementsystem/TourManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.windsnow1025.tourmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

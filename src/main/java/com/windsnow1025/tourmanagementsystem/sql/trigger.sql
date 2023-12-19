6133aa290f32394ed7b448f93a359497b4eda3d6
windsnow1025 <windsnow1025@gmail.com>
2023-12-19 03:56:43 +0000

    ”旅游信息“的费用触发器会因为没有“旅游线路_旅游时间段_旅游信息“无法触发

--- src/main/java/com/windsnow1025/tourmanagementsystem/sql/trigger.sql
+++ src/main/java/com/windsnow1025/tourmanagementsystem/sql/trigger.sql
@@ -1,8 +1,6 @@
 # 旅游费用_after_insert_旅游信息
-DELIMITER $$
-
-CREATE TRIGGER 旅游费用_after_insert_旅游信息
-    AFTER INSERT
+CREATE TRIGGER 旅游费用_before_insert_旅游信息
+    BEFORE INSERT
     ON 旅游信息
     FOR EACH ROW
 BEGIN
@@ -14,31 +12,24 @@ BEGIN
              JOIN 旅游线路_旅游时间段_旅游信息 ON 旅游时间段.id = 旅游线路_旅游时间段_旅游信息.旅游时间段_id
     WHERE 旅游线路_旅游时间段_旅游信息.旅游信息_id = NEW.id;
 
-    UPDATE 旅游信息
-    SET 旅游费用 = 价格
-    WHERE id = NEW.id;
-END$$
-
-DELIMITER ;
+    SET NEW.旅游费用 = 价格;
+END;
 
 # 旅游合同_after_insert_旅游信息
-DELIMITER $$
-
-CREATE TRIGGER 旅游合同_after_insert_旅游信息
-    AFTER INSERT
+CREATE TRIGGER 旅游合同_before_insert_旅游信息
+    BEFORE INSERT
     ON 旅游信息
     FOR EACH ROW
 BEGIN
     DECLARE 旅游合同信息 JSON;
 
     SELECT JSON_OBJECT(
-                   '信息ID', 信息ID,
-                   '旅游时间', 旅游时间,
-                   '旅游费用', 旅游费用,
-                   '保险', 保险,
-                   '旅游团_id', 旅游团_id,
-                   '顾客_身份证号', 顾客_身份证号,
-                   '旅游线路_id', 旅游线路_id,
+                   '信息ID', NEW.id,
+                   '旅游时间', NEW.旅游时间,
+                   '旅游费用', NEW.旅游费用,
+                   '保险', NEW.保险,
+                   '旅游团_id', NEW.旅游团_id,
+                   '顾客_身份证号', NEW.顾客_身份证号,
                    '地点', 地点,
                    '景点', 景点,
                    '时间段', 时间段,
@@ -50,9 +41,5 @@ BEGIN
     FROM 旅游信息视图
     WHERE 信息ID = NEW.id;
 
-    UPDATE 旅游信息
-    SET 旅游合同 = 旅游合同信息
-    WHERE id = NEW.id;
-END$$
-
-DELIMITER ;
+    SET NEW.旅游合同 = 旅游合同信息;
+END;

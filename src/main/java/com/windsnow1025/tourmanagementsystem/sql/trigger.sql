519fbfc7f322db8692eaaebb034fae95f04f9644
windsnow1025 <windsnow1025@gmail.com>
2023-12-19 04:06:12 +0000

    trigger on 旅游线路_旅游时间段_旅游信息

--- src/main/java/com/windsnow1025/tourmanagementsystem/sql/trigger.sql
+++ src/main/java/com/windsnow1025/tourmanagementsystem/sql/trigger.sql
@@ -1,35 +1,34 @@
-# 旅游费用_after_insert_旅游信息
-CREATE TRIGGER 旅游费用_before_insert_旅游信息
+CREATE TRIGGER 旅游费用_before_insert_旅游线路_旅游时间段_旅游信息
     BEFORE INSERT
-    ON 旅游信息
+    ON 旅游线路_旅游时间段_旅游信息
     FOR EACH ROW
 BEGIN
     DECLARE 价格 FLOAT;
 
-    SELECT 旅游时间段.价格
-    INTO 价格
+    SELECT 价格 INTO 价格
     FROM 旅游时间段
-             JOIN 旅游线路_旅游时间段_旅游信息 ON 旅游时间段.id = 旅游线路_旅游时间段_旅游信息.旅游时间段_id
-    WHERE 旅游线路_旅游时间段_旅游信息.旅游信息_id = NEW.id;
+    WHERE id = NEW.旅游时间段_id;
 
-    SET NEW.旅游费用 = 价格;
+    UPDATE 旅游信息
+    SET 旅游费用 = 价格
+    WHERE id = NEW.旅游信息_id;
 END;
 
-# 旅游合同_after_insert_旅游信息
-CREATE TRIGGER 旅游合同_before_insert_旅游信息
-    BEFORE INSERT
-    ON 旅游信息
+
+CREATE TRIGGER 旅游合同_after_insert_旅游线路_旅游时间段_旅游信息
+    AFTER INSERT
+    ON 旅游线路_旅游时间段_旅游信息
     FOR EACH ROW
 BEGIN
     DECLARE 旅游合同信息 JSON;
 
     SELECT JSON_OBJECT(
-                   '信息ID', NEW.id,
-                   '旅游时间', NEW.旅游时间,
-                   '旅游费用', NEW.旅游费用,
-                   '保险', NEW.保险,
-                   '旅游团_id', NEW.旅游团_id,
-                   '顾客_身份证号', NEW.顾客_身份证号,
+                   '信息ID', 旅游信息_id,
+                   '旅游时间', 旅游时间,
+                   '旅游费用', 旅游费用,
+                   '保险', 保险,
+                   '旅游团_id', 旅游团_id,
+                   '顾客_身份证号', 顾客_身份证号,
                    '地点', 地点,
                    '景点', 景点,
                    '时间段', 时间段,
@@ -39,7 +38,9 @@ BEGIN
            )
     INTO 旅游合同信息
     FROM 旅游信息视图
-    WHERE 信息ID = NEW.id;
+    WHERE 信息ID = NEW.旅游信息_id;
 
-    SET NEW.旅游合同 = 旅游合同信息;
+    UPDATE 旅游信息
+    SET 旅游合同 = 旅游合同信息
+    WHERE id = NEW.旅游信息_id;
 END;

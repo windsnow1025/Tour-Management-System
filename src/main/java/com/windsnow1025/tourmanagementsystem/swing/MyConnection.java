598edb9dc192ea9b7f7eded8259802095a44efe6
windsnow1025 <windsnow1025@gmail.com>
2023-12-19 08:38:17 +0000

    Update MyConnection.java

--- src/main/java/com/windsnow1025/tourmanagementsystem/swing/MyConnection.java
+++ src/main/java/com/windsnow1025/tourmanagementsystem/swing/MyConnection.java
@@ -1,24 +1,14 @@
 package com.windsnow1025.tourmanagementsystem.swing;
 
+import com.windsnow1025.tourmanagementsystem.db.JDBCHelper;
+
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class MyConnection {
-    private static final String HOST_ADDRESS="localhost";
-    private static final String DATABASE_NAME="tour";
-    private static final String USER="root";
-    private static final String PASSWORD="Fx021219";
-    private static final String DBURL="jdbc:mysql://"+HOST_ADDRESS+":3306/"+DATABASE_NAME+"?useSSL=false&serverTimezone=UTC";
-    public static Connection getConnection(){
-        Connection connection =null;
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            connection=java.sql.DriverManager.getConnection(DBURL,USER,PASSWORD);
-        } catch (ClassNotFoundException | SQLException e) {
-            throw new RuntimeException(e);
-        }
-        return connection;
+    public static Connection getConnection() throws SQLException {
+        JDBCHelper jdbcHelper = new JDBCHelper();
+        return jdbcHelper.getConnection();
     }
 
 }

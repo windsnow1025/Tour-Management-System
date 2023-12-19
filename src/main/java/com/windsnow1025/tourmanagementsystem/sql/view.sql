82fd01712234e7c1cd627e1992db85012eacf0c5
windsnow1025 <windsnow1025@gmail.com>
2023-12-19 03:37:22 +0000

    add 4 views

--- src/main/java/com/windsnow1025/tourmanagementsystem/sql/view.sql
+++ src/main/java/com/windsnow1025/tourmanagementsystem/sql/view.sql
@@ -25,3 +25,44 @@ FROM 旅游信息
          JOIN 旅游线路_旅游时间段_旅游信息 ON 旅游信息.id = 旅游线路_旅游时间段_旅游信息.旅游信息_id
          JOIN 旅游线路视图 ON 旅游线路_旅游时间段_旅游信息.旅游线路_id = 旅游线路视图.线路ID
          JOIN 旅游时间段 ON 旅游线路_旅游时间段_旅游信息.旅游时间段_id = 旅游时间段.id;
+
+
+CREATE VIEW 旅游线路收入视图 AS
+SELECT 旅游线路.id            AS 线路ID,
+       地点.地点,
+       景点.景点,
+       SUM(旅游信息.旅游费用) AS 总收入
+FROM 旅游线路
+         JOIN 地点 ON 旅游线路.id = 地点.旅游线路_id
+         JOIN 景点 ON 旅游线路.id = 景点.旅游线路_id
+         JOIN 旅游线路_旅游时间段_旅游信息 ON 旅游线路.id = 旅游线路_旅游时间段_旅游信息.旅游线路_id
+         JOIN 旅游信息 ON 旅游线路_旅游时间段_旅游信息.旅游信息_id = 旅游信息.id
+GROUP BY 旅游线路.id;
+
+
+CREATE VIEW 顾客消费视图 AS
+SELECT 顾客.身份证号,
+       SUM(旅游信息.旅游费用) AS 总消费
+FROM 顾客
+         JOIN 旅游信息 ON 顾客.身份证号 = 旅游信息.顾客_身份证号
+GROUP BY 顾客.身份证号;
+
+
+CREATE VIEW 导游业绩视图 AS
+SELECT 导游员工.身份证号,
+       旅游团.id              AS 旅游团_id,
+       SUM(旅游信息.旅游费用) AS 总业绩
+FROM 导游员工
+         JOIN 旅游团 ON 导游员工.旅游团_id = 旅游团.id
+         JOIN 旅游信息 ON 旅游团.id = 旅游信息.旅游团_id
+GROUP BY 导游员工.身份证号;
+
+
+CREATE VIEW 分公司业绩视图 AS
+SELECT 旅游分公司.id,
+       旅游分公司.分公司名,
+       SUM(导游业绩视图.总业绩) AS 总业绩
+FROM 旅游分公司
+         JOIN 旅游团 ON 旅游分公司.id = 旅游团.分公司_id
+         JOIN 导游业绩视图 ON 旅游团.id = 导游业绩视图.旅游团_id
+GROUP BY 旅游分公司.id;
